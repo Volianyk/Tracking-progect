5c904b54f801cf9efd3e49c8d92ef07fc65d4ee9
Volodymyr Volianyk <wrspct@gmail.com>
2022-09-04 17:06:23 +0300

    Custom  error handling functionality  was added

---
+++ src/main/java/com/job/tracking/service/exception/TaskNotFoundException.java
@@ -0,0 +1,15 @@
+package com.job.tracking.service.exception;
+
+import com.job.tracking.service.model.enums.ErrorType;
+
+public class TaskNotFoundException extends ServiceException{
+    private static String DEFAULT_MESSAGE="Task is not found";
+
+    public TaskNotFoundException() {
+        super(DEFAULT_MESSAGE);
+    }
+    @Override
+    public ErrorType getErrorType(){
+        return ErrorType.VALIDATION_ERROR_TYPE;
+    }
+}
